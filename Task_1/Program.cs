0192c9b5fcbeee87f9c437678e2857f8360ab28a
Kris362 <burdanova.kristina@gmail.com>
2023-02-25 19:37:06 +0300

    добавила задачи

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Подсчитать сумму цифр в числе*/
+
+Console.Clear ();
+Console.WriteLine ("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int s = 0;
+    while (a > 0)
+    {
+    s = s + a % 10;
+    a = a /10 ;
+    }
+ Console.WriteLine(s);
+
+
